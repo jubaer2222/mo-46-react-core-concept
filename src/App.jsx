03f6d90194c6b71e0db1457dad6da6f184a4d5ed
Jubaer Hossen <jubaerhossen042@gmail.com>
2025-09-03 15:12:39 +0600

    unControlled Field

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import ControlledField from './components/ControlledField/ControlledField'
 import FormAction from './components/FormAction/FormAction'
 import SimpleForm from './components/SimpleForm/SimpleForm'
+import UnControlledField from './components/UnControlledField/UnControlledField'
 
 function App() {
   
@@ -13,7 +14,8 @@ function App() {
       <h1>Explore React Form</h1>
       {/* <SimpleForm></SimpleForm> */}
       {/* <FormAction></FormAction> */}
-      <ControlledField></ControlledField>
+      {/* <ControlledField></ControlledField> */}
+      <UnControlledField></UnControlledField>
     </>
   )
 }
