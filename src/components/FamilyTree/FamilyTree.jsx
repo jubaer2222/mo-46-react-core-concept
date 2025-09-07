605abaa36b285348e777e86909a1d29d2f943a2a
Jubaer Hossen <jubaerhossen042@gmail.com>
2025-09-07 07:04:25 +0600

    final commit

--- src/components/FamilyTree/FamilyTree.jsx
+++ src/components/FamilyTree/FamilyTree.jsx
@@ -1,10 +1,13 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
 import Grandpa from './Grandpa';
 import './family-tree.css'
 
 export const AssetContext = createContext('');
+export const MoneyContext = createContext(0);
 
 const FamilyTree = () => {
+
+  const [money, setMoney] = useState(0);
 
   const asset = 'diamond';
   const newAsset = 'gold';
@@ -12,9 +15,12 @@ const FamilyTree = () => {
   return (
     <div className='family-tree'>
       <h2>Family Tree</h2>
-      <AssetContext.Provider value={newAsset}>
+      <h4>Total Family Money : {money}</h4>
+     <MoneyContext value={[money, setMoney]}>
+       <AssetContext.Provider value={newAsset}>
         <Grandpa asset={asset}></Grandpa>
       </AssetContext.Provider>
+     </MoneyContext>
     </div>
   );
 };
